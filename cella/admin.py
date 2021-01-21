bfbe4f4e669066127dc4a7811442838279a2e405
mnogoruk <lionless072@gmail.com>
2021-01-22 02:09:23 +0300

    Register new models

--- cella/admin.py
+++ cella/admin.py
@@ -1,26 +1,38 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import *
+from .models import (Operator,
+                     ResourceProvider,
+                     Resource,
+                     ResourceCost,
+                     ResourceAmount,
+                     ResourceAction,
+                     SpecificationCategory,
+                     Specification,
+                     SpecificationPrice,
+                     SpecificationAction,
+                     ResourceSpecification,
+                     ResourceSpecificationAssembled,
+                     Order,
+                     OrderSpecification,
+                     OrderAction)
 
 admin.site.register(Operator)
 
 admin.site.register(ResourceProvider)
 admin.site.register(Resource)
 admin.site.register(ResourceCost)
-
-admin.site.register(ResourceStorageAction)
-admin.site.register(ResourceCostAction)
-admin.site.register(ResourceServiceAction)
+admin.site.register(ResourceAmount)
+admin.site.register(ResourceAction)
 
 admin.site.register(SpecificationCategory)
 admin.site.register(Specification)
+admin.site.register(SpecificationPrice)
+admin.site.register(SpecificationAction)
+
 admin.site.register(ResourceSpecification)
+admin.site.register(ResourceSpecificationAssembled)
 
-admin.site.register(SpecificationCaptureAction)
-admin.site.register(SpecificationCoefficientAction)
-admin.site.register(SpecificationServiceAction)
-
-
-admin.site.register(UnverifiedCost)
-admin.site.register(UnresolvedProduct)
+admin.site.register(Order)
+admin.site.register(OrderSpecification)
+admin.site.register(OrderAction)
