e000bbe607f3c7d16b30ff778d4efeb759323af8
mnogoruk <lionless072@gmail.com>
2021-02-04 00:32:37 +0300

    deploy third

--- TraductorCella/wsgi.py
+++ TraductorCella/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TraductorCella.settings")
 
 application = get_wsgi_application()
 
