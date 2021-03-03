1b74f972cfd1c323ed56517a63955270476e83da
mnogoruk <lionless072@gmail.com>
2021-03-03 14:50:15 +0300

    no pagination limit

--- utils/pagination.py
+++ utils/pagination.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 20000
     page_size_query_param = 'page_size'
-    max_page_size = 2000
+    max_page_size = 200000
